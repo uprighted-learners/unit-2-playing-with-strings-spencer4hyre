4d5474683ac7412525f932f38a578bd774c963e2
spencer4hyre <163073659+spencer4hyre@users.noreply.github.com>
2024-03-26 13:13:37 -0400

    playing-with-strings-spencer4hyre

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+console.log("All dogs are good dogs".length)
+
+console.log("Apple"[3])
+
+console.log("titanic".toUpperCase())
+
+console.log("QUIETLY".toLowerCase())
+
+console.log("Java".repeat(10))
+
+console.log("berry".charAt(1))
+
+console.log("berry".charAt(0))
+
+console.log("banana".includes("nan"))
+
+console.log("banana".endsWith("ana"))
+
+console.log("blueberry".replace("blue", "black"))
+
+console.log("drive" + "way")
+
+console.log('Java' + "Script")
+
+console.log("Bert's pal Ernie" + ' sings "Rubber Ducky"')
+
+//Putting '' inside a "" string or vice versa causes JS to treat it like a quote inside the string.
+
+//Adding a '' string to a "" string causes JS to treat both as a string, allowing for concantenation and other functions.
+
+//Interior '' inside a string with not interrupt the output as long as the '' is closed inside the respective string.
+
+console.log(`Two plus two is ${2 + 2}`)
